8bd5661f4fda195cc0d5a2210a55d578fb0736a8
Jonathan Skarda <fjas;dljf>
2023-09-07 14:43:27 -0600

    Q2:  Create and activate the Python environment

---
+++ q2/create_environmet.ps1
@@ -0,0 +1,8 @@
+conda create --name py3vDev python=3.11.4
+# Adding “python” to the command creates a copy of the python
+# interpreter in the environment.  This helps with automatic
+# configuration of some tools.  For example, VS Code will only detect
+# a conda environment automatically if it contains a copy of the
+# interpreter.
+
+conda activate py3vDev
